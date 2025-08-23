ecdd7c9a079b647f18826a8ba79632438ac8bfdf
Teah Levi Torsween <teahlevitorsween@gmail.com>
2025-08-23 17:15:42 +0100

    dartwork

---
+++ ex1.dart
@@ -0,0 +1,8 @@
+
+
+/* print numbers from 1 to 10*/
+void main(){
+for (int i=1; i<=10; i++){
+print(i);
+}
+}
---
+++ ex5.dart
@@ -0,0 +1,7 @@
+void main(){
+  int sum=0;
+for( int i=1; i<=10; i++){
+sum +=i;
+}
+print("sum of numbers from 1 to 10: $sum");
+}
